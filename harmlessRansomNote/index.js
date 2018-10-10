fb333c5eee9a1165805a8d5dd25f8e62892b5d92
gogolab <m4ttek@gmail.com>
2018-10-10 14:47:57 +0200

    finished harmless ransom note alg

---
+++ harmlessRansomNote/index.js
@@ -0,0 +1,35 @@
+function harmlessRansomNote(noteText, magazineText) {
+    const noteArr = noteText.split(" ");
+    const magazineArr = magazineText.split(" ");
+    const magazineObj = {};
+
+    magazineArr.forEach(word => {
+        if (!magazineObj[word]) {
+            magazineObj[word] = 0;
+        }
+
+        magazineObj[word]++;
+    });
+
+    let noteIsPossible = true;
+
+    noteArr.forEach(word => {
+        if (!magazineObj[word]) {
+            noteIsPossible = false;
+            return;
+        } else {
+            magazineObj[word]--;
+
+            if (magazineObj[word] < 0) {
+                noteIsPossible = false;
+                return;
+            }
+        }
+    });
+
+    return noteIsPossible;
+}
+
+console.log(
+    harmlessRansomNote("test", "this is some test to test if function works")
+);
