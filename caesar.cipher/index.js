5bc26400c6f35fc9462ad3ea0da14855a8db03a2
gogolab <m4ttek@gmail.com>
2018-10-10 16:16:39 +0200

    finished palindromes and caesar cipher algs

---
+++ caesar.cipher/index.js
@@ -0,0 +1,32 @@
+function caesarCipher(str, num) {
+    const alphabet = "abcdefghijklmnopqrstuvwxyz";
+    let result = "";
+    num = num % alphabet.length;
+
+    str.split("").forEach(char => {
+        if (char === " ") {
+            result += char;
+            return;
+        }
+
+        const alphabetIndex = alphabet.indexOf(char.toLowerCase());
+
+        let cipheredLetterIndex = alphabetIndex + num;
+
+        if (cipheredLetterIndex > alphabet.length - 1) {
+            cipheredLetterIndex = cipheredLetterIndex - alphabet.length;
+        } else if (cipheredLetterIndex < 0) {
+            cipheredLetterIndex = cipheredLetterIndex + alphabet.length;
+        }
+
+        if (char.match(/[A-Z]/)) {
+            result += alphabet[cipheredLetterIndex].toUpperCase();
+        } else {
+            result += alphabet[cipheredLetterIndex];
+        }
+    });
+
+    return result;
+}
+
+console.log(caesarCipher("doM zaz", 1));
---
+++ palindromes/index.js
@@ -0,0 +1,19 @@
+function isPalindrome(string) {
+    // return (
+    //     string.toLowerCase() ===
+    //     string
+    //         .toLowerCase()
+    //         .split("")
+    //         .reverse()
+    //         .join("")
+    // );
+
+    const cleanArr = string
+        .toLowerCase()
+        .split("")
+        .filter(char => char.match(/[a-z]/));
+
+    return cleanArr.join("") === cleanArr.reverse().join("");
+}
+
+console.log(isPalindrome("rotoR"));
