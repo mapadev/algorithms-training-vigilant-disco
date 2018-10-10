a85652e137a79c1b67c8acd6e326ff7fd8b0c8bd
gogolab <m4ttek@gmail.com>
2018-10-10 20:09:20 +0200

    finished reverse array in place alg

---
+++ reverse.array/index.js
@@ -0,0 +1,17 @@
+// function reverseArrayInPlace(arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//         arr.splice(i, 0, arr.pop());
+//     }
+
+//     return arr;
+// }
+
+function reverseArrayInPlace(arr) {
+    for (let i = 0; i < arr.length / 2; i++) {
+        [arr[i], arr[arr.length - 1 - i]] = [arr[arr.length - 1 - i], arr[i]];
+    }
+
+    return arr;
+}
+
+console.log(reverseArrayInPlace([1, 2, 3, 4, 5]));
