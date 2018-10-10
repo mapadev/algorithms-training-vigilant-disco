95bbd4cea300e68ddf46e98aade691f70cab216c
gogolab <m4ttek@gmail.com>
2018-10-10 18:28:11 +0200

    finished reverse words alg

---
+++ reverse.words/index.js
@@ -0,0 +1,30 @@
+// function reverseWords(str) {
+//     wordsArr = str.split(" ");
+
+//     return wordsArr
+//         .map(word => {
+//             return word
+//                 .split("")
+//                 .reverse()
+//                 .join("");
+//         })
+//         .join(" ");
+// }
+
+function reverseWords(str) {
+    wordsArr = str.split(" ");
+
+    return wordsArr
+        .map(word => {
+            let reversedWord = "";
+
+            for (let char of word.split("")) {
+                reversedWord = char + reversedWord;
+            }
+
+            return reversedWord;
+        })
+        .join(" ");
+}
+
+console.log(reverseWords("hello world"));
