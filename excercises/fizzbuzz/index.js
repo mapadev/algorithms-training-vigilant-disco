ec1d20c4dcccd3ad6bd1b7f6a8d13c14d789a1e4
gogolab <m4ttek@gmail.com>
2018-10-11 12:51:41 +0200

    added tests, added defaults

--- excercises/fizzbuzz/index.js
+++ excercises/fizzbuzz/index.js
@@ -1,4 +1,4 @@
-function fizzbuzz(num, fizzNum, buzzNum) {
+function fizzBuzz(num, fizzNum = 3, buzzNum = 5) {
     for (let i = 1; i <= num; i++) {
         if (i % fizzNum === 0 && i % buzzNum === 0) {
             console.log("fizzbuzz");
@@ -12,4 +12,6 @@ function fizzbuzz(num, fizzNum, buzzNum) {
     }
 }
 
-fizzbuzz(20, 3, 5);
+fizzBuzz(20, 3, 5);
+
+module.exports = fizzBuzz;
